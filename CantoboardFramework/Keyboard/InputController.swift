3c871d94bd5fe684d872a56e739d09c4817e1d90
Alex Man <alex-the-man@users.noreply.github.com>
2021-03-18 16:08:53 -0700

    Remove Rime special chars on enter.

--- CantoboardFramework/Keyboard/InputController.swift
+++ CantoboardFramework/Keyboard/InputController.swift
@@ -305,7 +305,7 @@ class InputController {
     }
     
     private func insertComposingText(appendBy: String? = nil, shouldDisableSmartSpace: Bool = false) -> Bool {
-        if var composingText = inputEngine.composition?.text.filter({ $0 != " " }),
+        if var composingText = inputEngine.composition?.text.filter({ $0 != " " && !$0.isRimeSpecialChar }),
            !composingText.isEmpty {
             if let c = appendBy { composingText.append(c) }
             insertText(composingText, shouldDisableSmartSpace: shouldDisableSmartSpace)
