851b4cc061790621431ff3d0c3e75cd810bc0ad1
Alex Man <alex-the-man@users.noreply.github.com>
2021-05-07 16:54:08 -0700

    Move logging related code to a separate class.

--- CantoboardFramework/Keyboard/KeyboardViewController.swift
+++ CantoboardFramework/Keyboard/KeyboardViewController.swift
@@ -10,41 +10,10 @@ import UIKit
 
 import CocoaLumberjackSwift
 
-private class LogFormatter: NSObject, DDLogFormatter {
-    let dateFormatter: DateFormatter
-
-    override init() {
-        dateFormatter = DateFormatter()
-        dateFormatter.formatterBehavior = .behavior10_4
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss:SSS"
-
-        super.init()
-    }
-    
-    func format(message logMessage: DDLogMessage) -> String? {
-        let dateAndTime = dateFormatter.string(from: logMessage.timestamp)
-        return "\(dateAndTime) [\(logMessage.fileName):\(logMessage.line)]: \(logMessage.message)"
-    }
-}
-
 open class KeyboardViewController: UIInputViewController {
     private static let isLoggerInited = initLogger()
     
     private let c = InstanceCounter<KeyboardViewController>()
-        
-    private static func initLogger() -> Bool {
-        DDLog.add(DDOSLogger.sharedInstance) // Uses os_log
-
-        let fileLogger: DDFileLogger = DDFileLogger() // File Logger
-        fileLogger.rollingFrequency = 60 * 60 * 24 // 24 hours
-        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
-        fileLogger.logFormatter = LogFormatter()
-        DDLog.add(fileLogger)
-        
-        DDASLLogCapture.start()
-        
-        return true
-    }
     
     private var inputController: InputController?
     private(set) weak var keyboardView: KeyboardView?
