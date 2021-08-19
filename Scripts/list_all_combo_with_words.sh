6b91bfe18810e97c9dc9285d10fedb6ec32ac42f
Alex Man <alex-the-man@users.noreply.github.com>
2021-08-19 15:26:29 -0700

    Fix script path.

--- Scripts/list_all_combo_with_words.sh
+++ Scripts/list_all_combo_with_words.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat ../RimeSchema/Cantonese/jyut6ping3.dict.yaml | cut -d"	" -f2 | grep -v " " | sort -u | tee /tmp/combo_with_words.txt
+cat ../CantoboardFramework/Data/Rime/jyut6ping3.dict.yaml | cut -d"	" -f2 | grep -v " " | sort -u | tee /tmp/combo_with_words.txt
