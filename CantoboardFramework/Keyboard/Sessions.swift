288a02d7dd2a251643b7cd9d24ff74b34d991dd4
Alex Man <alex-the-man@users.noreply.github.com>
2021-09-10 18:30:00 -0700

    Fix currency to not show ¤ if locale isn't specified.

--- CantoboardFramework/Keyboard/Sessions.swift
+++ CantoboardFramework/Keyboard/Sessions.swift
@@ -15,18 +15,21 @@ public struct SessionState: Codable, Equatable {
     private static let defaultInputMode: InputMode = .mixed
     private static let defaultPrimarySchema: RimeSchema = .jyutping
     private static let defaultCharForm: CharForm = .traditionalHK
-    
+    private static let defaultCurrency = "$"
+
     public let currencySymbol: String = getLocalCurrencyCode()
     
     private static func getLocalCurrencyCode() -> String {
         if let currencySymbol = Locale.current.currencySymbol {
+            if currencySymbol == "¤" { return Self.defaultCurrency }
+            
             let currencySymbolWithoutLetters = currencySymbol.filter({ $0.isSymbol })
             if currencySymbolWithoutLetters.isEmpty {
                 return currencySymbol
             }
             return currencySymbolWithoutLetters
         }
-        return "$"
+        return Self.defaultCurrency
     }
     
     public var lastInputMode: InputMode {
