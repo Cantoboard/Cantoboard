fe6b8c8f6c835356e2174a4925f780017d4b5bde
Alex Man <alex-the-man@users.noreply.github.com>
2021-09-27 13:37:22 -0700

    Change RimeFramework build script to build XCFramework

--- RimeFramework/build.sh
+++ RimeFramework/build.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
+BUILD_DIR=build
+
 if [ -z "$LIBRIME_ROOT" ]; then
 echo "Please set LIBRIME_ROOT to point to the librime dir."
 exit 0
 fi
 
+mkdir build || true
+
 LIB_NAME=Rime
 DYLIBS="$LIBRIME_ROOT/dist/lib/librime.1.7.3.dylib"
 SOURCE_INFO_PLIST="src/Info.plist"
-OUT_DIR="."
+OUT_DIR=$BUILD_DIR
 
 # set OUT_DIR and LIB_NAME
 FW_PATH="$OUT_DIR/$LIB_NAME.framework"
@@ -23,3 +27,26 @@ install_name_tool -id @rpath/$LIB_NAME.framework/$LIB_NAME "$OUT_DYLIB"
 
 # set the DYLIBS and SOURCE_INFO_PLIST for DSYM
 dsymutil "$OUT_DYLIB" --out "$FW_PATH.dSYM"
+
+# Convert Framework to XCFramework
+rm -rf "$OUT_DIR/iphoneos" || true
+rm -rf "$OUT_DIR/iphonesimulator" || true
+mkdir -p "$OUT_DIR/iphoneos"
+mkdir -p "$OUT_DIR/iphonesimulator"
+
+IPHONE_OS_FW_PATH="$OUT_DIR/iphoneos/$LIB_NAME.framework"
+IPHONE_SIM_FW_PATH="$OUT_DIR/iphonesimulator/$LIB_NAME.framework"
+
+cp -R $FW_PATH "$IPHONE_OS_FW_PATH"
+cp -R $FW_PATH "$IPHONE_SIM_FW_PATH"
+
+IPHONE_OS_FW_LIB_PATH="$IPHONE_OS_FW_PATH/$LIB_NAME"
+IPHONE_SIM_FW_LIB_PATH="$IPHONE_SIM_FW_PATH/$LIB_NAME"
+
+echo "Spliting fat libs..."
+xcrun lipo -remove x86_64 "$IPHONE_OS_FW_LIB_PATH" -o "$IPHONE_OS_FW_LIB_PATH"
+xcrun lipo -remove arm64 "$IPHONE_SIM_FW_LIB_PATH" -o "$IPHONE_SIM_FW_LIB_PATH"
+
+echo "Creating XCFramework..."
+XCFW_PATH="$BUILD_DIR/$LIB_NAME.xcframework"
+xcodebuild -create-xcframework -framework "$IPHONE_OS_FW_PATH"/ -framework "$IPHONE_SIM_FW_PATH"/ -output "$XCFW_PATH"
